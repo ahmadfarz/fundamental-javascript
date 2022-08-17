2739ca838c9a36b4fb711e2b450dd84b2c0b27ca
Ahmad Farizi <ahmadfarizii17@gmail.com>
2022-08-17 21:45:32 +0700

    Variable or binding in javascript

--- mini-project-1/starter/app.js
+++ mini-project-1/starter/app.js
@@ -1,28 +1,16 @@
+var name = 'Wegodev';
+var greeting = 'Hello, ';
+
+// nama variable harus merepresentasikan isi variable
+var returnString = 'return';
+
+/** Tidak boleh digunakan
+ * var x = "wegodev programming school"
+ * var y = 2000
+ */
+
+console.log(greeting + name);
+
 const projectTitle = document.querySelector('.project-title');
 
 projectTitle.textContent = 'Title baru dari JS file';
-
-var thisIsFunction = 'This is function';
-const PI = 3.14;
-
-var y = 10;
-var z = 5;
-var x = y + z;
-
-// function di bawah ini akan return angka dari hasil penambahan
-function sumNum(a, b) {
-  return a + b;
-}
-
-var iniSebuahVar = 'var';
-
-/**
- ini adalah comment
- dengan baris
- lebih dari
- satu
- */
-
-// IniSebuahVar = 'variable baru';
-
-console.log(iniSebuahVar);
