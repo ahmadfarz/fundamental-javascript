dfca1dfbf9b47bba42c26da941819098e6d37174
Ahmad Farizi <ahmadfarizii17@gmail.com>
2022-08-17 21:07:31 +0700

    Formatting Code in Javascript

--- mini-project-1/starter/app.js
+++ mini-project-1/starter/app.js
@@ -1,3 +1,18 @@
-const heading = document.querySelector('.heading-title');
+const projectTitle = document.querySelector('.project-title');
 
-heading.innerHTML = 'testing 12310';
+projectTitle.textContent = 'Title baru dari JS file';
+
+var thisIsFunction = 'This is function';
+const PI = 3.14;
+
+var x = y + z;
+
+function sumNum(a, b) {
+  return a + b;
+}
+
+var iniSebuahVar = 'var';
+
+iniSebuahVar = 'Variable baru';
+
+var array = ['bubur ayam', 'kacang goreng', 3, 'bubur kacang ijo', '2000', 1999];
